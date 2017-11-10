f9ed2cafcbc3ba425d06c16740b1f6070eb11520
Odin Dutton <odindutton@gmail.com>
2017-11-11 10:27:41 +1100

    Add initial app object and loop

---
+++ script/application.js
@@ -0,0 +1,35 @@
+var throttle = function(type, name, obj) {
+  obj = obj || window;
+  var running = false;
+  var func = function() {
+    if (running) { return; }
+    running = true;
+    requestAnimationFrame(function() {
+      obj.dispatchEvent(new CustomEvent(name));
+      running = false;
+    });
+  };
+  obj.addEventListener(type, func);
+};
+
+throttle("resize", "optimizedResize");
+
+function main() {
+  var app = new PIXI.Application(800, 600, {backgroundColor : 0x000000});
+  document.body.appendChild(app.view);
+
+  app.renderer.view.style.position = "absolute";
+  app.renderer.view.style.display = "block";
+  app.renderer.autoResize = true;
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+
+  window.addEventListener("optimizedResize", function() {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+  });
+
+  app.ticker.add(function(delta) {
+    // TODO
+  });
+};
+
+document.addEventListener("DOMContentLoaded", main)
