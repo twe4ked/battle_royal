e0b2ade19d8178e7ff6d8572dc857f7cd516e14c
Odin Dutton <odindutton@gmail.com>
2017-11-11 15:58:49 +1100

    Clean up player init

--- script/application.js
+++ script/application.js
@@ -108,10 +108,13 @@ function setup() {
 
   renderInitialTiles();
 
-  player = {x: 0, y: 0, vx: 0, vy: 0, id: Math.random()}
-
-  player.x = app.renderer.width / 2;
-  player.y = app.renderer.height / 2;
+  player = {
+    x: app.renderer.width / 2,
+    y: app.renderer.height / 2,
+    vx: 0,
+    vy: 0,
+    id: Math.random()
+  }
 
   app.stage.addChild(playerSprites);
 
